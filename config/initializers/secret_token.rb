af18c25f9d7f889d2f3835e2c6257921eade096c
Joel Tan <lomyril86@gmail.com>
2012-08-22 20:57:34 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jumpstart::Application.config.secret_token = '2ae0c95a388b83ec5eac30fc83cbfccc15e7256e20335bb6a277d23b55d2948f0cecdba8fb2849b7afa23ca7bb4d191ce896c6046086f4dc7185521b0e7f0978'
